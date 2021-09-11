808a2f37de955d2de9a102c7be3000700e164a00
Nathan Santos <58302377+NSantos6@users.noreply.github.com>
2021-09-11 16:52:24 -0300

    Create calc.pl

---
+++ calc.pl
@@ -0,0 +1,20 @@
+#!/usr/bin/perl
+
+$n1 = 20;
+$n2 = 10;
+
+
+$calc = $n1 + $n2;
+print 'Resultado de $n1 + $n2 = ' . $calc . "\n";
+
+$calc = $n1 - $n2;
+print 'Resultado de $n1 - $n2 = ' . $calc . "\n";
+
+$calc = $n1 * $n2;
+print 'Resultado de $n1 * $n2 = ' . $calc . "\n";
+
+$calc = $n1 / $n2;
+print 'Resultado de $n1 / $n2 = ' . $calc . "\n";
+
+$calc = $n1 % $n2;
+print 'Resultado de $n1 % $n2 = ' . $calc. "\n";
